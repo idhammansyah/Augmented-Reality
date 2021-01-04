3d9955f21ed7e6cfebd5c9d07294677638319261
Idham Mansyah Awwalu <59715715+idhammansyah@users.noreply.github.com>
2021-01-04 10:22:09 +0700

    Add files via upload
    READ ME!
    
    CARA MENDOWNLOAD APLIKASI TOUR TO SUNDANESELAND
    
    1. BUKA LINK DI GDRIVE -> https://bit.ly/3atsNJh 
    2. APABILA ANDA TIDAK MEMPUNYAI IMAGE TARGET, BISA DI DOWNLOAD LANGSUNG DI LINK TERSEBUT ATAU DARI APLIKASI
    3. DOWNLOAD APLIKASI TERSEBUT YANG MEMILIKI FORMAT [.APK]
    4. APABILA SUDAH BERHASIL, INSTALL APLIKASI TERSEBUT
    5. JIKA SUDAH BERHASIL INSTALL, BUKA APLIKASI SAMPAI KEBAGIAN TAMPILAN AWAL APLIKASI
    6. JIKA SUDAH MEMILIKI IMAGE TARGET, PILIH BUTTON "PLAY", JIKA BELUM, PILIH "DOWNLOAD"
    7. IZINKAN APLIKASI UNTUK MEMBUKA KAMERA ANDA, LALU ARAHKAN APLIKASI KE GAMBAR YANG TERTERA DI IMAGE TARGET
    8. SELAMAT MENCOBA ^v^
    
    Nama Kelompok : DamHouseProject
    Anggota : 
    - Idham Mansyah Awwalu 	1402017067
    - Muhammad Randi	1402017105
    
    ENJOY THE GAMEEEEEE!
    
    *NOTE : 
    1. Aplikasi tidak dapat di share disini dikarenakan file melebihi 25MB dari batas yang github berikan, 
    2. Licensi gambar, video sudah di berikan didalam file "lisensi all document.txt"

---
+++ SceneChanges.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanges : MonoBehaviour
+{
+    public void Scene0() {  
+        SceneManager.LoadScene("Scene0");  
+    }
+    public void Scene1() {
+        SceneManager.LoadScene("Scene1");
+    }  
+}
---
+++ download.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class download : MonoBehaviour   {
+
+    public void OpenURL() {
+          Application.OpenURL("https://bit.ly/3atsNJh");
+          Debug.Log("Download the link");
+    }
+}
